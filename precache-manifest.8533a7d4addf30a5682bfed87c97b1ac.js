47d11b0ab9905d423fce7e6fb79503d5f3a1693e
AmoroAbd <amoroabd@gmail.com>
2020-10-18 15:25:09 +0000

    Updates

---
+++ precache-manifest.8533a7d4addf30a5682bfed87c97b1ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa95e487177152f696a83ea2bef4278e",
+    "url": "/Premest-Assignment1/index.html"
+  },
+  {
+    "revision": "960fcc01326da71a1046",
+    "url": "/Premest-Assignment1/static/css/main.15fa3a15.chunk.css"
+  },
+  {
+    "revision": "d726f649415c5ce052e5",
+    "url": "/Premest-Assignment1/static/js/2.180bcab9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Premest-Assignment1/static/js/2.180bcab9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "960fcc01326da71a1046",
+    "url": "/Premest-Assignment1/static/js/main.93824da1.chunk.js"
+  },
+  {
+    "revision": "98cb13e022087c98aec8",
+    "url": "/Premest-Assignment1/static/js/runtime-main.7f13fb94.js"
+  },
+  {
+    "revision": "10be56ebb4ec82bb3f9de11831677cef",
+    "url": "/Premest-Assignment1/static/media/belt.10be56eb.jpeg"
+  },
+  {
+    "revision": "ba7f2db4bbd639e24254057a6cbd8367",
+    "url": "/Premest-Assignment1/static/media/image1.ba7f2db4.jpeg"
+  }
+]);
